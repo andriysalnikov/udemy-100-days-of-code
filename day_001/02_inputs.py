292eb520a1759abe7a3ad80b210fb79bad8cf318
Andriy Salnikov <a.salnikov.home@gmail.com>
2026-08-04 15:34:37 +0200

    Complete Day 001

---
+++ day_001/02_inputs.py
@@ -0,0 +1,5 @@
+# This is comment...
+input("What is your name?")
+
+# This line will take a name and print it
+print("Hello " + input("What is your name? ") + "!")
---
+++ day_001/03_variables.py
@@ -0,0 +1,30 @@
+# Variables...
+name = input("What is your name? ")
+print("Hello " + name + "!")
+print()
+
+name = "User"
+print(name)
+name = "Another User"
+print(name)
+print()
+
+# Function 'len()'
+# print(len(input("What is your name? ")))
+print()
+
+username = input("What is your name? ")
+length = len(username)
+print(length)
+print()
+
+glass1 = "milk"
+glass2 = "juice"
+temp = glass1
+glass1 = glass2
+glass2 = temp
+print(glass1)
+print(glass2)
+print()
+
+# !!!!! Use good naming in "snake case"
