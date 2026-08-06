2e8f6a308ca11ae4a10d3616a8e403313874a71a
Andriy Salnikov <a.salnikov.home@gmail.com>
2026-08-06 09:31:04 +0200

    Complete Day 006

---
+++ day_006/02_while_loops.py
@@ -0,0 +1,11 @@
+# while condition_is_true:
+#     do_something()
+#     do_something_again()
+
+def jump():
+    print("Jump!")
+
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -= 1
